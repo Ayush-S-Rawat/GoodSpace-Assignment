bef28d34fbd647d5505dc1296bf3f1a4dde71775
Ayush-S-Rawat <ayushrawat772002@gmail.com>
2024-09-14 14:54:42 +0530

    expense page outline

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.scss'
+import Below from './components/Below/below'
+import Feedback from './components/Feedback/Feedback'
+import Footer from './components/Footer/Footer'
+import HeroSection from './components/HeroSection/HeroSection'
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <HeroSection/>
+      <div className='big-screen'>
+        <Feedback/>
+        <Below/> 
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
